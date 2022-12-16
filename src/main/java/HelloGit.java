8fd7a77f43b29b965c01951a3c6007bc30b342e9
xhbh <hsk@pku.edu.com>
2022-12-16 18:04:37 +0800

    first commit

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName HelloGit
+ * @Description TODO
+ * @Author 小黄不黄
+ * @Date 2022/12/16 18:03
+ * @Version 1.0
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
